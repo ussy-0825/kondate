d23a81f113f61bcfe8907ad04d7064225a212e09
ussy-0825 <uy.advance@gmail.com>
2021-03-01 19:21:42 +0900

    レシピ詳細閲覧、レシピ修正機能の実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,6 +13,23 @@ class RecipesController < ApplicationController
     end
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def recipe_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'menus#index'
-  resources :recipes, only: [:new, :create]
+  resources :recipes, only: [:new, :create, :show, :edit, :update]
   resources :menus, only: [:new, :create]
 end
