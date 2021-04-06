0d89f7ba73909de7751efdbc39a00dbdce48fe7c
ussy-0825 <uy.advance@gmail.com>
2021-04-06 22:35:08 +0900

    ゲストログイン機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = 'gestpass'
+      user.name = 'ゲスト'
+    end
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root to: 'menus#index'
   resources :recipes
   resources :menus do
